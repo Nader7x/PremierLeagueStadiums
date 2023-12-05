d896fde4fa98a286f64fc5b7ea86bfffa70c4291
Ibrahim Gad <yehya2154@gmail.com>
2023-12-05 02:00:40 +0200

    no_nader

---
+++ models/personModel.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+mongoose.set('strictQuery', false);
+
+const baseOptions = {
+    discriminatorKey: "type",
+    collection: "person",
+};
+
+const personSchema = new mongoose.Schema({
+    name:{type:String,required:true},
+    age:{type:Number,required:true,max:100,min:1},
+},baseOptions);
+const Person = mongoose.model("Person",personSchema);
+
+module.exports = Person;
---
+++ models/persons.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const Person = require('./personModel')
+
+const Referee = Person.discriminator("Referee",new mongoose.Schema({
+    nationality:String
+}));
+
+const Coach = Person.discriminator("Coach",new mongoose.Schema({
+    nationality:String
+}));
+
+const Commentator = Person.discriminator("Commentator",new mongoose.Schema({
+    nationality:String
+}));
+
+//admin, user, player same collection or should be in different one
+const User = Person.discriminator("User",new mongoose.Schema({
+    username:{type:String,required:true},
+    password:{type:String,required:true},
+}));
+
+const Admin = Person.discriminator("Admin",new mongoose.Schema({
+    username:{type:String,required:true},
+    password:{type:String,required:true},
+}));
+
+const positions = ['gk','cb','lb','rb','cm','cam','cdm','cf','rw','rm','lw','lm','st']
+
+const Player = Person.discriminator("Player",new mongoose.Schema({
+    nationality:String,
+    kitNumber:{type:Number,required:true,max:99,min:1},
+    position:{type:String, enum: positions},
+    team:{type:mongoose.Schema.Types.ObjectId, ref: 'Team',required:false}
+}));
+
+
+module.exports = {
+    Referee,Coach,Commentator,User,Admin,Player
+}
+/*calling it like this
+const myModule = require('./myModule.js');
+const method = myModule.method;
+const otherMethod = myModule.otherMethod;
+// OR:
+const {method, otherMethod} = require('./myModule.js');
+ */
